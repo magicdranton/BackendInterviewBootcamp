da0ef2c45cf35526633cf3646eea9554885eb7da
A T <tsybin@distantlite.com>
2026-08-19 14:43:49 +0500

    LeetCode task 1768

---
+++ src/CSharp_Tasks/CLeetCode_Tasks.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp_Tasks
+{
+    public class CLeetCode_Tasks
+    {
+        public static string MergeAlternately_1768(string word1, string word2)
+        {
+            StringBuilder sb = new StringBuilder(210);
+            int charPtr = Math.Min(word1.Length, word2.Length);
+
+            // Mix letters
+            for (int i = 0; i < charPtr; i++)
+            {
+                sb.Append(word1[i]);
+                sb.Append(word2[i]);
+            }
+
+            // Append word's tail
+            if (charPtr < word1.Length)
+            {
+                sb.Append(word1.Substring(charPtr, word1.Length - charPtr));
+            }
+
+            if (charPtr < word2.Length)
+            {
+                sb.Append(word2.Substring(charPtr, word2.Length - charPtr));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
