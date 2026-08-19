41eab078174b0dd8a93d07e4a2e7f91d45eac16f
A T <tsybin@distantlite.com>
2026-08-19 15:57:39 +0500

    timing optimization here: SB can extract substrings w/o instantiating them

--- src/CSharp_Tasks/CLeetCode_Tasks.cs
+++ src/CSharp_Tasks/CLeetCode_Tasks.cs
@@ -8,25 +8,25 @@ namespace CSharp_Tasks
     {
         public static string MergeAlternately_1768(string word1, string word2)
         {
-            StringBuilder sb = new StringBuilder(210);
-            int charPtr = Math.Min(word1.Length, word2.Length);
+            StringBuilder sb = new StringBuilder(word1.Length + word2.Length);
+            int minLen = Math.Min(word1.Length, word2.Length);
 
             // Mix letters
-            for (int i = 0; i < charPtr; i++)
+            for (int i = 0; i < minLen; i++)
             {
                 sb.Append(word1[i]);
                 sb.Append(word2[i]);
             }
 
             // Append word's tail
-            if (charPtr < word1.Length)
+            if (minLen < word1.Length)
             {
-                sb.Append(word1.Substring(charPtr, word1.Length - charPtr));
+                sb.Append(word1, minLen, word1.Length - minLen);
             }
 
-            if (charPtr < word2.Length)
+            if (minLen < word2.Length)
             {
-                sb.Append(word2.Substring(charPtr, word2.Length - charPtr));
+                sb.Append(word2, minLen, word2.Length - minLen);
             }
 
             return sb.ToString();
